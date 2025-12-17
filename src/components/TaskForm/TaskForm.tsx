32082e28862876bad6f6a09344417d2892176f30
Mag <166319982+magali-la@users.noreply.github.com>
2025-12-17 11:30:18 -0500

    feat(task form): create task form component with controlled inputs, use states, and styling

---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+export default function TaskForm() {
+    // states for controlled inputs
+    const [title, setTitle] = useState<string>('');
+    const [desc, setDesc] = useState<string>('');
+    const [dueDate, setDueDate] = useState<string>('');
+    const [priority, setPriority] = useState<'high' | 'medium' | 'low' | ''>('');
+
+    // // function to check every state is not length 0 before adding the task
+    // function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    //     event.preventDefault();
+
+    //     if (title.length === 0 || ){
+            
+    //     } else {
+
+    //     }
+    // }
+
+    return (
+        <form className="flex flex-col gap-4 py-8 px-8 md:px-10 sm:w-[50vw] bg-banana rounded-2xl border-3 border-shadow" onSubmit={(event) => event.preventDefault()} aria-label="add a new task to your list">
+            <h2 className="text-2xl font-semibold">Add a Task</h2>
+            {/* title input */}
+            <label htmlFor="taskTitle" className="font-medium">Task Title</label>
+            <input type="text" id="taskTitle" value={title} onChange={(event) => setTitle(event.target.value)}/>
+            {/* description input */}
+            <label htmlFor="taskDesc" className="font-medium">Description</label>
+            <textarea rows={6} id="taskDesc" value={desc} onChange={(event) => setDesc(event.target.value)}/>
+            {/* due date input */}
+            <label htmlFor="taskDue" className="font-medium">Due Date</label>
+            <input type="date" id="taskDue" value={dueDate} onChange={(event) => setDueDate(event.target.value)}/>
+            {/* priority radio group */}
+            <label htmlFor="taskPriority" className="font-medium">Priority Level</label>
+            <div id="taskPriority" className="flex flex-row gap-12" role="radiogroup">
+                <label htmlFor="priorityHigh">
+                    <input id="priorityHigh" type="radio" role="radio" aria-checked="false" value="high" checked={priority === 'high'} onChange={() =>setPriority('high')}/> High
+                </label>
+                <label htmlFor="priorityMedium">
+                    <input id="priorityMedium" type="radio" role="radio" aria-checked="false" value="medium" checked={priority === 'medium'} onChange={() =>setPriority('medium')}/> Medium
+                </label>
+                <label htmlFor="priorityLow">
+                    <input id="priorityLow" type="radio" role="radio" aria-checked="false" value="low" checked={priority === 'low'} onChange={() =>setPriority('low')}/> Low
+                </label>
+            </div>
+            {/* add button */}
+            <button type="submit" role="button" className="w-fit py-2 px-6 bg-plum/80 hover:bg-plum rounded-2xl transition-colors duration-500 cursor-pointer">Add Task</button>
+        </form>
+    )
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
+import TaskForm from "../components/TaskForm/TaskForm";
 import TaskList from "../components/TaskList/TaskList";
 
 export default function Dashboard() {
     return (
         <div>
+            <TaskForm />
             <TaskList/>
         </div>
     )
